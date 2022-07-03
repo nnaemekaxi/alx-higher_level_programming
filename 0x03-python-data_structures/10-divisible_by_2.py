3ce4595209e9c3b7f1587cdb5e94f4bae79db30e
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-07-03 10:17:53 -0700

    multiples

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    # a function that finds all multiples of 2 in a list
+    new_list = []
+    if my_list:
+        for element in my_list:
+            new_list.append(False if element % 2 else True)
+    return 
