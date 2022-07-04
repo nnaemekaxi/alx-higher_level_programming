c7f55b83ee9130038d9bf22a39159047f259da21
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-07-04 09:01:37 -0700

    python list

---
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -0,0 +1,25 @@
+#include "Python.h"
+
+void print_python_list_info(PyObject *p)
+{
+  PyListObject *list;
+  Py_ssize_t size, x;
+  PyObject *object;
+  struct _typeobject *type;
+
+  if (strcmp(p->ob_type->tp_name, "list") == 0)
+    {
+      list = (PyListObject *)p;
+      size = list->ob_base.ob_size;
+      printf("[*] Size of the Python List = %ld\n", size);
+      printf("[*] Allocated = %ld\n", list->allocated);
+      x = 0;
+      while (x < size;)
+	{
+	  object = list->ob_item[x];
+	  type = object->ob_type;
+	  printf("Element %ld: %s\n", x, type->tp_name);
+	x++;
+	}
+    }
+}
