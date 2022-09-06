a1a02858de726d12cd1f2d787daa3156461a06f4
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-09-06 14:30:11 -0700

    temp

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,3 @@
---this script retrieves average temperature
-SELECT city, avg(value) AS avg_temp FROM temperatures 
-GROUP BY city 
-ORDER BY avg_temp DESC;
+-- Import in hbtn_0c_0 database
+-- Write a script that displays the average temperature (Fahrenheit) by city ordered by temperature (descending).
+SELECT city, AVG(value) as avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
