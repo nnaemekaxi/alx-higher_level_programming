95bc8128133cac6268524719c8a53fdf76eca3f5
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-09-06 14:02:06 -0700

    count 89

---
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -0,0 +1,4 @@
+-- this script displays number of records with id == 89 in 'first_table'
+-- of db 'hbtn_0c_0'
+-- db name will be passed as arg to mysql cmd
+SELECT COUNT(*) FROM first_table WHERE id = 89;
