f06d355672bf6b23902f3d93eb7d30a99eac89cc
Nnaemeka Joseph <nnaemekaxi@yahoo.com>
2022-10-21 12:35:40 -0700

    status

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,7 +5,7 @@ same as 0-hbtn_status with requests model
 
 if __name__ == '__main__':
     import requests
-    html = requests.get('https://intranet.hbtn.io/status')
+    html = requests.get('https://alx-intranet.hbtn.io/status')
     print("Body response:")
     print("\t- type: {}".format(html.text.__class__))
     print("\t- content: {}".format(html.text))
